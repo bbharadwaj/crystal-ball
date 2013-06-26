c9e0891ebe9366afac3ac0aba0ccf18c804ff155
brijesh_bharadwaj <brijesh@example.com>
2013-06-26 18:02:35 +0530

    hmm

--- src/com/example/crystallball/MainActivity.java
+++ src/com/example/crystallball/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.crystallball;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
 	private CrystalBall mCrystalBall = new CrystalBall();
+	private TextView mAnswerLabel;
+	private Button mGetAnswerButton;
+	private ImageView mCystalBallImage;
+	
 	
 
     @Override
@@ -17,27 +26,65 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //Declare View Variables and assign them views from the layout file
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-        Button getAnswerButton = (Button)findViewById(R.id.button1);
+        //assign view variables from the layout file
+        mAnswerLabel = (TextView) findViewById(R.id.textView1);
+        mGetAnswerButton = (Button)findViewById(R.id.button1);
+        mCystalBallImage = (ImageView) findViewById(R.id.imageView1);
         
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+        mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
+				
 				//button was clicked, so give an answer
+				
 				String answer = mCrystalBall.getAnAnswer();
+				mAnswerLabel.setText(answer);
 				
-				
-				answerLabel.setText(answer);
-				
-				
-				
+				animateCrystalBall();
+				animateAnswer();
+				playSound();
 				
 			}
 		});
         
         
+    }
+    
+    private void animateCrystalBall(){
+    	
+    	mCystalBallImage.setImageResource(R.drawable.ball_animation);
+    	AnimationDrawable ballAnimation = (AnimationDrawable) mCystalBallImage.getDrawable();
+    	if (ballAnimation.isRunning()){
+    		ballAnimation.stop();
+    	}
+    		ballAnimation.start();
+    	
+    }
+    
+    private void animateAnswer(){
+    	AlphaAnimation fadeInAnimation = new AlphaAnimation (0,1);
+    	fadeInAnimation.setDuration(1500);
+    	fadeInAnimation.setFillAfter(true);
+    	mAnswerLabel.setAnimation(fadeInAnimation);
+    	
+    	
+    }
+    
+    private void playSound(){
+    	MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+    	player.start();
+    	player.setOnCompletionListener(new OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+				
+			}
+		
+    });
+    		
+    	
     }
 
 
